7f13d6199bf4a2af5f1ef9b84eb5c453ebfcec0d
BienIsOver00/31 <Fabio.grandi553@gmail.com>
2025-07-30 15:32:18 +0200

    Add files via upload

---
+++ Poundstokilo.py
@@ -0,0 +1,13 @@
+Pounds =input("Enter your Pounds, write 0 if you do not know")
+Kilos =input("Enter your Kilos, write 0 if you do not know")
+
+Pounds = float(Pounds)
+Kilos = float(Kilos)
+
+Weight_in_Kilos = Pounds * 0.45
+Weight_in_Pounds = Kilos * 2.2
+
+print("Weight in Pounds: " , Weight_in_Pounds)
+print("Weight in Kilos: " , Weight_in_Kilos)
+
+
